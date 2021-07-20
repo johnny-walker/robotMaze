26b7225fa0931066f78273150d874cf0763d1d92
johnny-walker <albert_hsu@cyberlink.com>
2021-07-20 10:14:47 +0800

    add maploader

--- index.js
+++ index.js
@@ -1,49 +1,13 @@
 const express = require('express')
-const fs = require('fs')
-const csv = require('csv')
+const {queryMap} = require('./src/mapLoader.js') 
 
 var app = express()
-var record = ['0' * 20]
-let map = []
 
-// the __dirname is the location of index.js
-const fsPromises = fs.promises;
-async function loadMap() {
-    try {
-        // STEP 1: 讀取 CSV 檔
-        const inputFile = __dirname + '/res/map/map00.csv'
-        const input = await fsPromises.readFile(inputFile)
-
-        // STEP 2：建立讀出 CSV 用的陣列和 parser
-        const parser = csv.parse({ delimiter: ',' })
-
-        // STEP 3-1：建立對應事件 - 讀取資料
-        parser.on('readable', function () {
-            while ((record = parser.read())) {
-                map.push(record);
-            }
-        })
-
-        // STEP 3-2：錯誤處理
-        parser.on('error', function (err) {
-            console.error(err.message)
-        })
-
-        // STEP 3-3：取得最後 output 的結果
-        parser.on('end', function () {
-            console.log('total records:', map.length)
-        })
-
-        // STEP 4：放入預備讀取的內容
-        parser.write(input)
-
-        // STEP 5：關閉 readable stream
-        parser.end()
-    } catch (error) {
-        console.log('error', error)
-    }
-}
-loadMap()
+var map = []
+const inputMap = __dirname + '/res/map/map00.csv'
+queryMap(inputMap, (response)=> {
+    map = response
+})
 
 app.get('/', function (req, res) {
     console.log(req.url)
---
+++ src/mapLoader.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const csv = require('csv')
+
+
+const fsPromises = fs.promises
+let map = []
+let record = ['0' * 20]
+
+async function loadMap(inputFile, calbak) {
+    try {
+        // STEP 1: 讀取 CSV 檔
+        const input = await fsPromises.readFile(inputFile)
+
+        // STEP 2：建立讀出 CSV 用的陣列和 parser
+        const parser = csv.parse({ delimiter: ',' })
+
+        // STEP 3-1：建立對應事件 - 讀取資料
+        parser.on('readable', function () {
+            while ((record = parser.read())) {
+                map.push(record);
+            }
+        })
+
+        // STEP 3-2：錯誤處理
+        parser.on('error', function (err) {
+            console.error(err.message)
+        })
+
+        // STEP 3-3：取得最後 output 的結果
+        parser.on('end', function () {
+            console.log('total records:', map.length)
+            calbak(map)
+        })
+
+        // STEP 4：放入預備讀取的內容
+        parser.write(input)
+
+        // STEP 5：關閉 readable stream
+        parser.end()
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+exports.queryMap = function queryMap(input, calbak) {
+    loadMap(input, calbak)
+}
